52add97210e2fcc37325e8ad564a6f88c01e40a7
truonghoang_11 <hoangdepzai346@gmail.com>
2026-08-17 11:36:54 +0700

    fix: update X (Twitter) profile URL

--- src/content/social.ts
+++ src/content/social.ts
@@ -2,5 +2,5 @@ export const social = [
   { url: "mailto:truongnguyenhoang0512@gmail.com", name: "mail" },
   { url: "https://github.com/hoangtruong01", name: "github" },
   { url: "https://www.linkedin.com/", name: "linkedin" },
-  { url: "https://x.com/", name: "x" },
+  { url: "https://x.com/zzhoangzzz", name: "x" },
 ] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
